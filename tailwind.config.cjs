3a74088789a6a40f6c9e89a6557101490beb898f
wesngu28 <serpents123@gmail.com>
2022-11-04 01:02:26 -0700

    images and configs

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+		  width: {
+			'75v': '75vh',
+		  },
+		  animation: {
+			slideshow: 'slideshow 1s ease-out',
+			spinonce: 'spin 1s linear once',
+		  },
+		  keyframes: {
+			slideshow: {
+			  '0%': {
+				opacity: 0,
+			  },
+			  '100%': {
+				opacity: 1,
+			  },
+			},
+		  },
+		},
+	  },
+	plugins: [],
+}
