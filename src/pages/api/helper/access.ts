56d17407f7794245d6d13c138b6ed37f599d2496
Wesley <serpents123@gmail.com>
2023-01-04 16:39:14 -0800

    remove unused packages

--- src/pages/api/helper/access.ts
+++ src/pages/api/helper/access.ts
@@ -1,20 +1,20 @@
-import querystring from 'query-string'
+import { URLSearchParams } from 'url'
 
 export const getAccessToken = async () => {
   const clientID = await import.meta.env.SPOTIFY_CLIENT_ID
   const clientSecret = await import.meta.env.SPOTIFY_CLIENT_SECRET
   const refreshToken = await import.meta.env.SPOTIFY_REFRESH_TOKEN
   const basicAuth = Buffer.from(`${clientID}:${clientSecret}`).toString('base64')
+  const params = new URLSearchParams()
+  params.append('grant_type', 'refresh_token')
+  params.append('refresh_token', refreshToken)
   const response = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
     headers: {
       Authorization: `Basic ${basicAuth}`,
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: querystring.stringify({
-      grant_type: 'refresh_token',
-      refresh_token: refreshToken,
-    }),
+    body: params,
   })
 
   return response.json()
