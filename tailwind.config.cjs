0314507a9915d1edc3e3fcece8787a88ccd3c765
wesngu28 <wesley@wesngu28.com>
2024-08-14 23:23:40 -0700

    use variable karla font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      'karla': ['Karla', 'sans-serif']
+      'karla': ['Karla Variable', 'sans-serif']
     },
     extend: {
       screens: {
